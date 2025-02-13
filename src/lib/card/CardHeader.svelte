658d40ec3963254e250d1f07724ab5047b74c98c
Michael Reinegger <reinemic2.0@gmail.com>
2025-02-13 14:16:18 +0100

    Add Card component with header, body, description, footer, and title subcomponents

---
+++ src/lib/card/CardHeader.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { cn } from '$lib/utility.js';
+
+	let {
+		children
+	}: {
+        children: Snippet;
+	} = $props();
+</script>
+
+<div class={cn('flex flex-col')}>
+	{@render children()}
+</div>
